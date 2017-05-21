84371906b16bb263e878a19d09561ee4d9065cdc
Pad <pad2g.vb@gmail.com>
2017-05-21 22:46:47 +0200

    Update offsets.c

--- nalu935/offsets.c
+++ nalu935/offsets.c
@@ -33,7 +33,7 @@ void init_offsets() {
     */
     /*@Cryptic - iPhone5,1(5), iPhone 5,2(5) iOS 9.3.5 Not Confirmed correct for all devices.*/
     if (strcmp(u.version, "Darwin Kernel Version 15.6.0: Fri Aug 19 10:37:55 PDT 2016; root:xnu-3248.61.1~1/RELEASE_ARM_S5L8950X") == 0) {
-        allproc_offset = 0x804602c8;
+        allproc_offset = 0x804602c8;  // gotta subtract kernel __TEXT:HEADER from that (x - 80001000 ?)
         rootvnode_offset = 0x8041a128;
     }
     
@@ -41,7 +41,14 @@ void init_offsets() {
     else if (strcmp(u.version, "Darwin Kernel Version 16.0.0: Fri Aug  5 22:15:30 PDT 2016; root:xnu-3789.1.24~11/RELEASE_ARM64_S5L8960X") == 0) {
         allproc_offset = 0x5a4128;
         rootvnode_offset = 0x5aa0b8;
-    }//some beta ios
+    }
+    
+    // iPad3,1 9.3.5 by @Pad2G
+    else if (strcmp(u.version, "Darwin Kernel Version 15.6.0: Fri Aug 19 10:37:55 PDT 2016; root:xnu-3248.61.1~1/RELEASE_ARM_S5L8945X") == 0) {
+        allproc_offset = 0x457170;
+        kernproc_offset = 0x411134;
+        rootvnode_offset = 0x411120;
+    }
     else {
         printf("missing offset, prob crashing\n");
     }
